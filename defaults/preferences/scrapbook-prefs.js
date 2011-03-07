87ed6633bd404a86d3ec99a66c313320a01b3233
Matthew Kelly <me@matkelly.com>
2011-03-06 21:08:30 -0500

    Initialize the repo with version 1.1 of archivefb

---
+++ defaults/preferences/scrapbook-prefs.js
@@ -0,0 +1,27 @@
+pref("extensions.{53A03D43-5363-4669-8190-99061B2DEBA6}.description", "chrome://archivefb/locale/about.properties");
+
+pref("archivefb.ui.contextMenu", true);
+pref("archivefb.ui.contextSubMenu", false);
+pref("archivefb.ui.bookmarkMenu", true);
+pref("archivefb.ui.menuBar", true);
+pref("archivefb.ui.menuBar.icon", false);
+pref("archivefb.ui.toolsMenu", false);
+pref("archivefb.ui.statusBar", true);
+pref("archivefb.ui.folderList", "");
+pref("archivefb.ui.folderList.maxEntries", 5);
+pref("archivefb.tabs.open", false);
+pref("archivefb.tabs.openSource", false);
+pref("archivefb.tabs.combinedView", true);
+pref("archivefb.tabs.searchResult", true);
+pref("archivefb.tabs.note", false);
+pref("archivefb.data.default", true);
+pref("archivefb.data.path", "");
+pref("archivefb.multibook.enabled", false);
+pref("archivefb.fileViewer.default", true);
+pref("archivefb.fileViewer.path", "");
+pref("archivefb.tree.unshift", false);
+pref("archivefb.tree.autoCollapse", false);
+pref("archivefb.confirmDelete", true);
+pref("archivefb.showDetailOnDrop", false);
+pref("archivefb.notifyOnComplete", true);
+
